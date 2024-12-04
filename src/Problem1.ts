8d0aa22c043b8d1ca8332a901584b95d608dc6f0
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2024-12-04 23:31:21 +0600

    first commit

---
+++ src/Problem1.ts
@@ -0,0 +1,1 @@
+// Design a TypeScript function repeatString that takes two parameters: a string and a number. The function should return a new string that repeats the input string the specified number of times.
